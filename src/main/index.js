1a9e44eb96ecdd0f38b977ca552d974792ca4123
Viktor Fagionato <viktormfagionato@gmail.com>
2022-04-07 15:44:21 -0300

    refactor: add logs when db connect and app running

--- src/main/index.js
+++ src/main/index.js
@@ -1,12 +1,18 @@
 const app = require('./config/app')
 const sequelize = require('../infra/database')
+const logger = require('../logger')
+
+const port = 3000
 
 const start = async () => {
   try {
-    await sequelize.autenticate()
-    app.listen(3000)
+    await sequelize.authenticate()
+    logger.info('database connected')
+    app.listen(port, () => {
+      logger.info(`API up on http://localhost:${port}/api`)
+    })
   } catch (error) {
-    console.error(error)
+    logger.error(error)
   }
 }
 
