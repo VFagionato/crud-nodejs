c28a296d0877ce79c94fea1679a05f465dcd27d1
Viktor Fagionato <viktormfagionato@gmail.com>
2022-04-07 18:50:56 -0300

    test: ensure call registerUserRepository with correct params

--- tests/presentation/routes/register-user.spec.js
+++ tests/presentation/routes/register-user.spec.js
@@ -1,25 +1,67 @@
 /* eslint-disable no-undef */
 const HttpResponse = require('../helpers/http-response')
 
+const makeRegisterUserRepository = () => {
+  class RegisterUserRepository {
+    async register ({ cpf, nome, telefone, email, setor = null }) {
+      this.cpf = cpf
+      this.nome = nome
+      this.telefone = telefone
+      this.email = email
+      this.setor = setor
+      return this.user
+    }
+  }
+  const registerUserRepository = new RegisterUserRepository()
+  registerUserRepository.user = {
+    id: 'valid_id',
+    cpf: '768.554.879-45',
+    email: 'joao@mail.com',
+    nome: 'João das Neves',
+    telefone: 45965675445,
+    setor: 'setor_id'
+  }
+  return registerUserRepository
+}
+
 const makeSut = () => {
-  return new RegisterUserRouter()
+  const registerUserRepositorySpy = makeRegisterUserRepository()
+  const sut = new RegisterUserRouter({ registerUserRepository: registerUserRepositorySpy })
+  return {
+    sut,
+    registerUserRepositorySpy
+  }
 }
 
 class RegisterUserRouter {
+  constructor ({ registerUserRepository } = {}) {
+    this.registerUserRepository = registerUserRepository
+  }
+
   async route (httpRequest) {
     if (!httpRequest.body) {
       return HttpResponse.badRequest('body not present in request')
     }
-    const { cpf, nome, telefone, email } = httpRequest.body
+    const { cpf, nome, telefone, email, setor } = httpRequest.body
     if (!cpf || !nome || !telefone || !email) {
       return HttpResponse.badRequest('body must contain at least: cpf, name, phone, email')
     }
+    const user = await this.registerUserRepository.register({ cpf, nome, telefone, email, setor })
   }
 }
 
 describe('Register User Router', () => {
+  const validRequest = {
+    body: {
+      cpf: '768.554.879-45',
+      email: 'joao@mail.com',
+      nome: 'João das Neves',
+      telefone: 45965675445
+    }
+  }
+
   test('should return 400 if httpRequest do not have a body', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {}
     const response = await sut.route(httpRequest)
     expect(response.statusCode).toBe(400)
@@ -27,36 +69,12 @@ describe('Register User Router', () => {
   })
 
   test('should return 400 if any require param is missing', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const requests = [].concat(
-      {
-        body: {
-          nome: 'Any Name',
-          telefone: 71967876548,
-          email: 'valid_email@mail.com'
-        }
-      },
-      {
-        body: {
-          cpf: '345.234.544-01',
-          telefone: 71967876548,
-          email: 'valid_email@mail.com'
-        }
-      },
-      {
-        body: {
-          cpf: '345.234.544-01',
-          nome: 'Any name',
-          email: 'valid_email@mail.com'
-        }
-      },
-      {
-        body: {
-          cpf: '345.234.544-01',
-          nome: 'Any name',
-          telefone: 71967876548
-        }
-      }
+      { body: { ...validRequest.body, cpf: undefined } },
+      { body: { ...validRequest.body, email: undefined } },
+      { body: { ...validRequest.body, nome: undefined } },
+      { body: { ...validRequest.body, telefone: undefined } }
     )
 
     for (const request of requests) {
@@ -64,5 +82,15 @@ describe('Register User Router', () => {
       expect(response.statusCode).toBe(400)
       expect(response.body).toBe('body must contain at least: cpf, name, phone, email')
     }
+  })
+
+  test('should call registerUserRepository with correct params', async () => {
+    const { sut, registerUserRepositorySpy } = makeSut()
+    await sut.route(validRequest)
+    expect(registerUserRepositorySpy.cpf).toBe(validRequest.body.cpf)
+    expect(registerUserRepositorySpy.nome).toBe(validRequest.body.nome)
+    expect(registerUserRepositorySpy.email).toBe(validRequest.body.email)
+    expect(registerUserRepositorySpy.telefone).toBe(validRequest.body.telefone)
+    expect(registerUserRepositorySpy.setor).toBe(null)
   })
 })
