80cf6b7351151f4edf583771a5b7accae852b3ff
Viktor Fagionato <viktormfagionato@gmail.com>
2022-04-08 14:51:19 -0300

    fix: add express.json in setup app

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const setupRoutes = require('./routes')
+const setupRoutes = require('./routes.js')
+require('../../infra/database')
+
+app.use(express.json())
 
 setupRoutes(app)
 
